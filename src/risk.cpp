80e636f3e8fb10b91b794ea745e8d1a9e5830418
allanpichardo <allan.pichardo@gmail.com>
2018-09-12 20:01:06 -0400

    modified task to build and run specific file open in editor

--- src/risk.cpp
+++ src/risk.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main() {
     /* This is the main entry point of the program */
-    cout << "It worked!\n";
+    cout << "When I grow up I'll be a game!\n";
     return 0;
 }

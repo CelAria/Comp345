72d10117508b6c78b8e060d7587e43c75ec1a3f4
allanpichardo <allan.pichardo@gmail.com>
2018-09-13 13:30:00 -0400

    modified task to use dedicated terminal in vscode

--- src/risk.cpp
+++ src/risk.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 int main() {
+    int blah;
     /* This is the main entry point of the program */
-    cout << "When I grow up I'll be a game!\n";
+    cout << "It worked!\n";
+    
     return 0;
 }

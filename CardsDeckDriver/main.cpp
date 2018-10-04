3be5d18e812ee1da4d445d2a200b549f53a3d82c
Martin-John Hearty <martinjohn.hearty@gmail.com>
2018-10-04 15:18:53 -0400

    Current Cards and deck

--- CardsDeckDriver/main.cpp
+++ CardsDeckDriver/main.cpp
@@ -6,8 +6,67 @@
 #include <string>
 using namespace std;
 
-int main(int argc, const char * argv[]) {
+int main()
+{
     srand(time(NULL));
-    Deck Deck;
+    Deck deck1;
+    Hand *hand1 = new Hand();
+    
+    const int initalNumOfCardsInDeck = deck1.getTotalCardsInDeck();
+    int infantryCounter = 0;
+    int artilleryCounter = 0;
+    int cavalryCounter = 0;
+    
+    for(int i=0; i < initalNumOfCardsInDeck; i++)
+    {
+        deck1.draw(hand1);
+        if(deck1.getCardTypeDrawn() == "infantry")
+            infantryCounter++;
+        if(deck1.getCardTypeDrawn() == "artillery")
+            artilleryCounter++;
+        if(deck1.getCardTypeDrawn() == "cavalry")
+            cavalryCounter++;
+    }
+    cout << "Number of infantry cards in deck: " + to_string(infantryCounter) << endl;
+    cout << "Number of artillery cards in deck: " + to_string(artilleryCounter) << endl;
+    cout << "Number of cavalry cards in deck: " + to_string(cavalryCounter) << endl;
+    
+    delete hand1;
+    hand1 = NULL;
+    
+    cout << "" << endl;
+    
+    Deck deck2;
+    Hand *hand2 = new Hand();
+    
+    infantryCounter = 0;
+    artilleryCounter = 0;
+    cavalryCounter = 0;
+    
+    for(int i=0; i < 9; i++)
+    {
+        deck2.draw(hand2);
+        if(deck2.getCardTypeDrawn() == "infantry")
+            infantryCounter++;
+        if(deck2.getCardTypeDrawn() == "artillery")
+            artilleryCounter++;
+        if(deck2.getCardTypeDrawn() == "cavalry")
+            cavalryCounter++;
+    }
+    cout << "Number of infantry cards in deck: " + to_string(infantryCounter) << endl;
+    cout << "Number of artillery cards in deck: " + to_string(artilleryCounter) << endl;
+    cout << "Number of cavalry cards in deck: " + to_string(cavalryCounter) << endl;
+    
+    cout << "" << endl;
+    
+    cout << "Exchange#1 armies recieved: " + to_string(hand2->exchange("1 of each Card Type")) << endl;
+    cout << "Exchange#2 armies recieved: " + to_string(hand2->exchange("3 Infantry Cards")) << endl;
+    cout << "Exchange#3 armies recieved: " + to_string(hand2->exchange("3 Artillery Cards")) << endl;
+    cout << "Exchange#4 armies recieved: " + to_string(hand2->exchange("3 Cavalry Cards")) << endl;
+    
+    cout << "Total number of armies: " + to_string(hand2->getTotalNumOfArmies()) << endl;
+    
+    delete hand2;
+    hand2 = NULL;
     return 0;
 }
--- Risk/cardsdeck.h
+++ Risk/cardsdeck.h
@@ -1,18 +1,57 @@
 #pragma once
+#include <string>
+using namespace std;
+
+class Hand {
+private:
+    int totalNumOfCardsInHand;
+    int numOfInfantryCards;
+    int numOfArtilleryCards;
+    int numOfCavalryCards;
+    int numOfArmies;
+    int totalNumberOfArmies;
+    
+public:
+    Hand();
+    int exchange(string s);
+    int getNumOfArmies(){ return numOfArmies; }
+    int getTotalNumOfArmies(){ return totalNumberOfArmies; }
+    void incrementNumOfArmies();
+    int getTotalCardsInHand(){ return totalNumOfCardsInHand; }
+    int getNumOfInfantryCards(){ return numOfInfantryCards; }
+    int getNumOfArtilleryCards(){ return numOfArtilleryCards; }
+    int getNumOfCavalryCards(){ return numOfCavalryCards; }
+    void infantryPlusOne(){ numOfInfantryCards++; totalNumOfCardsInHand++; }
+    void artilleryPlusOne(){ numOfArtilleryCards++; totalNumOfCardsInHand++; }
+    void cavalryPlusOne(){ numOfCavalryCards++; totalNumOfCardsInHand++; }
+    void infantryMinusOne(){ numOfInfantryCards--; totalNumOfCardsInHand--; }
+    void artilleryMinusOne(){ numOfArtilleryCards--; totalNumOfCardsInHand--; }
+    void cavalryMinusOne(){ numOfCavalryCards--; totalNumOfCardsInHand--; }
+    bool isHandEmpty();
+    bool handContainsOneOfEachCardType();
+    bool handContains3InfantryCards();
+    bool handContains3ArtilleryCards();
+    bool handContains3CavalryCards();
+};
 
 class Deck {
 private:
-    int totalNumberOfCards;
     int infantryCards;
     int artilleryCards;
     int cavalryCards;
+    int totalNumberOfCards;
+    string cardTypeBeingDrawn;
     
 public:
     Deck();
-    void draw();
-    void exchange();
-    int getTotalCardsInDeck();
-    int getNumOfInfantryCards();
-    int getNumOfArtilleryCards();
-    int getNumOfCavalryCards();
+    void draw(Hand *hand);
+    string getCardTypeDrawn(){ return cardTypeBeingDrawn; }
+    int getTotalCardsInDeck(){ return totalNumberOfCards; }
+    int getNumOfInfantryCards(){ return infantryCards; }
+    int getNumOfArtilleryCards(){ return artilleryCards; }
+    int getNumOfCavalryCards(){ return cavalryCards; }
+    void infantryCardMinusOne(){ infantryCards--; totalNumberOfCards--; }
+    void artilleryCardMinusOne(){ artilleryCards--; totalNumberOfCards--; }
+    void cavalryCardMinusOne(){ cavalryCards--; totalNumberOfCards--; }
+    int howManyCardTypesLeft();
 };

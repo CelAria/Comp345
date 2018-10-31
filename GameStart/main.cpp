5b6a40c8cb458674fd290198465987572cec19ab
Celeste <celestepimm@gmail.com>
2018-10-31 19:17:41 -0400

    Part 1/2 drivers

---
+++ GameStart/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameStart
+//
+//  Created by Celeste Pimm on 2018-10-31.
+//  Copyright © 2018 comp345. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

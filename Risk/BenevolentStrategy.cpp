39be64b90c01f7af3e4cbf8d283991973bc5c035
Matthew Salaciak <matt.salaciak@gmail.com>
2018-11-18 02:28:37 -0500

    added BenevolentStrategy

---
+++ Risk/BenevolentStrategy.cpp
@@ -0,0 +1,29 @@
+//
+//  BenevolentStrategy.cpp
+//  Risk
+//
+//  Created by Matthew Salaciak on 2018-11-18.
+//  Copyright © 2018 comp345. All rights reserved.
+//
+
+#include "BenevolentStrategy.h"
+#include "passiveAttack.h"
+#include "gamemap.h"
+
+void BenevolentStrategy::attack(Player *player, GameMap *gameMap) {
+    PassiveAttack playerAttack(player,gameMap);
+    playerAttack.attackLoop();
+}
+
+void BenevolentStrategy::fortify(Player *player, GameMap *gameMap) {
+    
+}
+
+void BenevolentStrategy::reinforce(Player *player, GameMap *gameMap) {
+    
+}
+
+int AggressiveStrategy::getType() {
+    return 2;
+}
+

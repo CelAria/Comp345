24090e816a931615faf6cf3c0a40d4195714430f
Martin-John Hearty <martinjohn.hearty@gmail.com>
2018-11-18 21:57:43 -0500

    Added comments to the reinforce files. So many comments, marker cannot take off points for comments haha

--- Risk/reinforceview.cpp
+++ Risk/reinforceview.cpp
@@ -1,13 +1,10 @@
+// Team project assignment #3
+// COMP 345: Advanced program design with C++, Fall 2018
+// Written by: Martin-John Hearty
 //
-//  reinforceview.cpp
-//  Risk
-//
-//  Created by Martin-John Hearty on 2018-11-15.
-//  Copyright © 2018 comp345. All rights reserved.
-//
+// ------------------------------------------------------
 
 #include "reinforceview.h"
-
 #include <iostream>
 
 using namespace std;
@@ -55,7 +52,7 @@ int ReinforceView::getNumOfArmiesToPlace(GameMap* gameMap, Player *player){
     }
     return numOfArmiesRecieved;
 }
-
+//display stats of the player'hand and number of countrues the player owns
 void ReinforceView::displayStats(int numOfArmiesRecieved, Player *player){
     cout << "Number of armies: " << numOfArmiesRecieved << endl;
     cout << "Player" << player->getPlayerId() << " currently controls " << player->getCountriesCount() << " countries and holds "
@@ -65,11 +62,13 @@ void ReinforceView::displayStats(int numOfArmiesRecieved, Player *player){
     << ", Artillery: " << player->getHand()->getNumOfArtilleryCards()
     << ", Cavalry: " << player->getHand()->getNumOfCavalryCards() << endl;
 }
-
+//exchange player's cards for armies
 void ReinforceView::userInputCardExchange(int numOfArmiesRecieved, Player *player){
+    //if the player has less than 5 cards call the exchange method.
     while(player->getCardsCount() > 5)
     {
         string userInput;
+        //print statments to ask the user which type of exchange the player whats to execute
         cout << endl;
         cout << "Card exchange (Enter either 0, 1, 2 or 3) " <<endl;
         cout << "\t• 0 = Exchange 3 cards of the same type" << endl;
@@ -80,6 +79,7 @@ void ReinforceView::userInputCardExchange(int numOfArmiesRecieved, Player *playe
         getline (cin, userInput);
         numOfArmiesRecieved = numOfArmiesRecieved + player->getHand()->exchange(userInput);
     }
+    //if the player still has more than 2 cards, ask the user if he or she wants to exchange one more time
     if((player->getCardsCount() > 2)){
         string userInput;
         cout << "Do you want to do a card exchange? y/n -> ";
@@ -87,6 +87,7 @@ void ReinforceView::userInputCardExchange(int numOfArmiesRecieved, Player *playe
         if(userInput == "y"){
             string userInput2;
             cout << endl;
+            //print statments to ask the user which type of exchange the player whats to execute
             cout << "Card exchange (Enter either 0, 1, 2 or 3) " <<endl;
             cout << "\t• 0 = Exchange 3 cards of the same type" << endl;
             cout << "\t• 1 = Exchange 3 Infantry cards" << endl;
@@ -100,6 +101,7 @@ void ReinforceView::userInputCardExchange(int numOfArmiesRecieved, Player *playe
     cout << endl;
 }
 
+//displays the countries that the player owns
 void ReinforceView::displayContriesPlayerOwns(vector<Country*> allCountries, Player *player){
     cout << "The number of armies placed in each country that player " << player->getPlayerId() << " controls: " << endl;
     //displays the number of armies in each country the player controls
@@ -108,29 +110,34 @@ void ReinforceView::displayContriesPlayerOwns(vector<Country*> allCountries, Pla
         cout << "\t• " << allCountries[i]->getArmiesCount() << " armies placed in " << allCountries[i]->getName() <<endl;
     }
 }
-
+//asks the user in which country the player wants to had his or hers armies
 bool ReinforceView::placeArmiesHuman(GameMap* gameMap, vector<Country*> allCountries, int numOfArmiesRecieved, Player *player){
     int numOfArmiesToPlace = numOfArmiesRecieved;
      cout << endl;
      cout << "The number of armies to be placed: " << numOfArmiesToPlace << endl;
-    
+    // do loop that loop through all the countries and if there are still armies left to place, repeat
     do{
+        //loop through all the countries the player owns
         for(int i = 0; i < allCountries.size(); i++)
         {
+            //ask the user how many armies the player wants to add to the a counrty
             string userInput;
             cout << "How many armies do you want to placed in " << allCountries[i]->getName() << "? -> ";
             getline (cin, userInput);
-            
+            //if the input is not a digit continue to the next iteration
             if(!isdigit(userInput[0])){
                 continue;
             }
+            //converts the input to an int
             int userInput2 = (userInput.at(0)-48);
+                //checks if the input is smaller than the number of that needs to be placed
                 if(userInput2 > numOfArmiesToPlace){
                     cout << "Input entered is greater than the number of armies left to place" << endl;
                 }else{
                     numOfArmiesToPlace -= userInput2;
                     gameMap->getCountry(allCountries[i]->getName())->setArmiesCount((gameMap->getCountry(allCountries[i]->getName())->getArmiesCount() + userInput2));
                 }
+                //break out of the loop if there is no more armies to be placed
                 if(numOfArmiesToPlace == 0){
                     break;
                 }

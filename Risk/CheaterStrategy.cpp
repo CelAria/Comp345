88611e58bac69e0d2730655a6e23e108b9b4c06e
Matthew Salaciak <matt.salaciak@gmail.com>
2018-11-27 17:46:17 -0500

    added random and cheater strategies

---
+++ Risk/CheaterStrategy.cpp
@@ -0,0 +1,28 @@
+//
+//  CheaterStrategy.cpp
+//  Risk
+//
+//  Created by Matthew Salaciak on 2018-11-27.
+//  Copyright © 2018 comp345. All rights reserved.
+//
+
+#include "CheaterStrategy.h"
+
+#include "gamemap.h"
+#include <vector>
+#include "country.h"
+
+void CheaterStrategy :: attack(Player* player, GameMap* gameMap){
+    
+}
+
+void CheaterStrategy ::fortify(Player* player, GameMap* gameMap){
+    
+}
+void CheaterStrategy :: reinforce(Player* player, GameMap* gameMap) {
+    
+}
+
+int CheaterStrategy :: getType() {
+    return 4;
+}

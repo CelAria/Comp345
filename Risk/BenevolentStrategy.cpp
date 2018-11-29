bba9349b5ed057b2997904a08ede6b827c160cf9
Allan Pichardo <allan.pichardo@gmail.com>
2018-11-29 12:53:40 -0500

    doing country check before executing fortify and reinforce

--- Risk/BenevolentStrategy.cpp
+++ Risk/BenevolentStrategy.cpp
@@ -24,21 +24,23 @@ void BenevolentStrategy::fortify(Player *player, GameMap *gameMap) {
     
     vector<Country*> countries = player->getAllCountries();
     
-    Country* weakestCountry = countries[0];
-    for(int i = 0; i < countries.size(); ++i) {
-        if(countries[i]->getArmiesCount() < weakestCountry->getArmiesCount()) {
-            weakestCountry = countries[i];
+    if(countries.size() > 0) {
+        Country* weakestCountry = countries[0];
+        for(int i = 0; i < countries.size(); ++i) {
+            if(countries[i]->getArmiesCount() < weakestCountry->getArmiesCount()) {
+                weakestCountry = countries[i];
+            }
         }
-    }
-    
-    cout << "Weakest country is:" << endl;
-    cout << "\t" << weakestCountry->toString() << endl << endl;
-    
-    for(int i = 0; i < countries.size(); ++i) {
-        if(countries[i] != weakestCountry && countries[i]->getArmiesCount() > 1) {
-            int amount = countries[i]->getArmiesCount() - 1;
-            cout << "Moving " << amount << " armies from " << countries[i]->getName() << " to " << weakestCountry->getName() << endl;
-            fortifyController.moveArmies(countries[i], weakestCountry, amount);
+        
+        cout << "Weakest country is:" << endl;
+        cout << "\t" << weakestCountry->toString() << endl << endl;
+        
+        for(int i = 0; i < countries.size(); ++i) {
+            if(countries[i] != weakestCountry && countries[i]->getArmiesCount() > 1) {
+                int amount = countries[i]->getArmiesCount() - 1;
+                cout << "Moving " << amount << " armies from " << countries[i]->getName() << " to " << weakestCountry->getName() << endl;
+                fortifyController.moveArmies(countries[i], weakestCountry, amount);
+            }
         }
     }
 }
@@ -46,14 +48,16 @@ void BenevolentStrategy::fortify(Player *player, GameMap *gameMap) {
 void BenevolentStrategy::reinforce(Player *player, GameMap *gameMap) {
     vector<Country*> countries = player->getAllCountries();
     
-    Country* weakestCountry = countries[0];
-    for(int i = 0; i < countries.size(); ++i) {
-        if(countries[i]->getArmiesCount() < weakestCountry->getArmiesCount()) {
-            weakestCountry = countries[i];
+    if(countries.size() > 0) {
+        Country* weakestCountry = countries[0];
+        for(int i = 0; i < countries.size(); ++i) {
+            if(countries[i]->getArmiesCount() < weakestCountry->getArmiesCount()) {
+                weakestCountry = countries[i];
+            }
         }
+        PassiveReinforceController reinforceController = PassiveReinforceController(player, gameMap);
+        reinforceController.start(weakestCountry);
     }
-    PassiveReinforceController reinforceController = PassiveReinforceController(player, gameMap);
-    reinforceController.start(weakestCountry);
 }
 
 int BenevolentStrategy::getType() {
--- Risk/aggressivestrategy.cpp
+++ Risk/aggressivestrategy.cpp
@@ -24,21 +24,23 @@ void AggressiveStrategy::fortify(Player *player, GameMap *gameMap) {
     
     vector<Country*> countries = player->getAllCountries();
     
-    Country* strongestCountry = countries[0];
-    for(int i = 0; i < countries.size(); ++i) {
-        if(countries[i]->getArmiesCount() > strongestCountry->getArmiesCount()) {
-            strongestCountry = countries[i];
+    if(countries.size() > 0) {
+        Country* strongestCountry = countries[0];
+        for(int i = 0; i < countries.size(); ++i) {
+            if(countries[i]->getArmiesCount() > strongestCountry->getArmiesCount()) {
+                strongestCountry = countries[i];
+            }
         }
-    }
-    
-    cout << "Strongest country is:" << endl;
-    cout << "\t" << strongestCountry->toString() << endl << endl;
-    
-    for(int i = 0; i < countries.size(); ++i) {
-        if(countries[i] != strongestCountry && countries[i]->getArmiesCount() > 1) {
-            int amount = countries[i]->getArmiesCount() - 1;
-            cout << "Moving " << amount << " armies from " << countries[i]->getName() << " to " << strongestCountry->getName() << endl;
-            fortifyController.moveArmies(countries[i], strongestCountry, amount);
+        
+        cout << "Strongest country is:" << endl;
+        cout << "\t" << strongestCountry->toString() << endl << endl;
+        
+        for(int i = 0; i < countries.size(); ++i) {
+            if(countries[i] != strongestCountry && countries[i]->getArmiesCount() > 1) {
+                int amount = countries[i]->getArmiesCount() - 1;
+                cout << "Moving " << amount << " armies from " << countries[i]->getName() << " to " << strongestCountry->getName() << endl;
+                fortifyController.moveArmies(countries[i], strongestCountry, amount);
+            }
         }
     }
 }
@@ -46,14 +48,16 @@ void AggressiveStrategy::fortify(Player *player, GameMap *gameMap) {
 void AggressiveStrategy::reinforce(Player *player, GameMap *gameMap) {
     vector<Country*> countries = player->getAllCountries();
     
-    Country* strongestCountry = countries[0];
-    for(int i = 0; i < countries.size(); ++i) {
-        if(countries[i]->getArmiesCount() > strongestCountry->getArmiesCount()) {
-            strongestCountry = countries[i];
+    if(countries.size() > 0) {
+        Country* strongestCountry = countries[0];
+        for(int i = 0; i < countries.size(); ++i) {
+            if(countries[i]->getArmiesCount() > strongestCountry->getArmiesCount()) {
+                strongestCountry = countries[i];
+            }
         }
+        AgressiveReinforceController reinforceController = AgressiveReinforceController(player, gameMap);
+        reinforceController.start(strongestCountry);
     }
-    AgressiveReinforceController reinforceController = AgressiveReinforceController(player, gameMap);
-    reinforceController.start(strongestCountry);
 }
 
 int AggressiveStrategy::getType() {
